d80e8192644909c75eafd4a082e13b205bd6a96a
NewMatheusED <matheus081105@gmail.com>
2022-09-09 22:40:39 -0300

    .

--- init.js
+++ init.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
     let info = document.createElement('div');
 
-    info.innerHTML = '<p>Oi minha vida!! Só quero te avisar que, no final desa página, tem as músicas que eu coloquei, e to aqui só pra falar que não deu tempo de fazer algo mais elaborado</p>'
+    info.innerHTML = '<p>Oi minha vida ♥ Só quero te avisar que, no final desa página, tem as músicas que eu coloquei, e to aqui só pra falar que não deu tempo de fazer algo mais elaborado ou esteticamente bonito, pode colocar a culpa toda no senac viu, ele quem fica ocupando meu tempo com um monte de trabalho</p>'
 
     alert.appendChild(info)
 }
